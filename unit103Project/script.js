264716e6a13f4b076cd0b3fb38ec5fff915d2974
Jakegu <34921463+Jakegu@users.noreply.github.com>
2023-06-21 15:56:24 -0700

    Add files via upload

---
+++ unit103Project/script.js
@@ -0,0 +1,61 @@
+//console.log("Script");
+
+//var user = "Jake";
+//var age = 99;
+//var isStudent = true;
+
+//var normalAge = age - 68;
+
+//console.log(user,age,isStudent);
+//document.write("<p> Name: " + user + "</p><p> Age:" + age + "</p><p> Active Student: " + isStudent);
+
+//document.write(`
+    //<p>Name: ${user}</p>
+    //<p>Age: ${normalAge}</p>
+    //<p>Active student: ${isStudent}</p>
+//`);
+
+//let jobTitle = "Pastry Chef";
+//let geoLocation = "Nashville";
+//let partnerName = "Caroline";
+//let numberChildren = 3;
+
+//document.write(`
+    //<p>You will be a ${jobTitle} in ${geoLocation},</p>
+    //<p>and married to ${partnerName} with ${numberChildren} kids.</p>
+//`);
+
+//let name = "Jake";
+//let email = "abc@gmail.com";
+//let password = "jakeissick007";
+//let age2 = 31;
+//let country= "'Murica";
+//let monthlySalary = 7000;
+
+//console.log(name,email,monthlySalary*12);
+
+//Web application variables
+
+let userName = "Jakeg";
+let password = "jakeissick007";
+let ip = "09864426279";
+let isSubscribed = true;
+let isActive = true;
+let email = "abc@gmail.com";
+let phoneNumber = "666-666-6666";
+let cardNumber = "**************";
+let city = "San Diego";
+let state = "CA";
+let birthday = "1/15/1992";
+let age = 31;
+let gender = "M";
+let langauge = "English";
+let account = "Business";
+let numMonthsSubbed = 23;
+let married = false;
+let numProfiles = 3;
+let familyAccount = true;
+let ageRegitrict = true;
+
+
+
